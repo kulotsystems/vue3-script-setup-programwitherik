d64fc1b0803db8141856d5d892393715ab68c74d
kulotsystems <kulotsystems@gmail.com>
2022-10-04 12:04:58 +0800

    Complete.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
+    <div>
+        <h3>Hello World</h3>
+        <h5>{{ name }}</h5>
 
+        <HelloWorld :name="name" @pressed="pressed" class="green"/>
+        <div v-if="pokemon">
+            <img :src="pokemon.sprites.back_default" alt="">
+        </div>
+    </div>
 </template>
 
-<style scoped>
 
+<script setup>
+    import { ref } from 'vue';
+    import HelloWorld from './components/HelloWorld.vue';
+
+    let name = 'kulotsystems';
+    let pokemon = ref(null);
+
+    async function pressed() {
+        const info = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        const json = await info.json();
+        pokemon.value = json;
+        console.log(pokemon.value);
+    }
+</script>
+
+
+<style>
+    #app {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>Hello from component.</h3>
+        <h5>Received name: {{ props.name }}</h5>
+
+        <button @click="getAlert">Press Me</button>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        name: String
+    });
+
+    const emits = defineEmits(['pressed']);
+
+    const getAlert = () => {
+        emits('pressed', 'Whatever');
+    };
+</script>
+
+
+<style scoped>
+    .green {
+        background-color: green;
+        color: white;
+        padding: 15px;
+    }
+</style>
